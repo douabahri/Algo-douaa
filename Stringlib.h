104d4b7d1f9ed50b4f08bb0927f7c66cca7bc3eb
douabahri <douaba024@gmail.com>
2025-10-29 13:26:08 +0100

    Create Stringlib.h

---
+++ Stringlib.h
@@ -0,0 +1,11 @@
+#ifndef STRINGLIB_H
+#define STRINGLIB_H
+
+char *LoadString(int N);
+int StringLength(char *str);
+void ReverseArray(char arr[], char rev[], int n);
+void DisplayArray(char arr[]);
+int SumStringASCII(char *p);
+void ReverseString(char *start, char *end);
+
+#endif
